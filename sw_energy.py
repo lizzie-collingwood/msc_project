39bb8ca77aaa7370f2afa2a161f95ac42a808532
Colin Cotter <colin.cotter@imperial.ac.uk>
2022-04-28 15:15:21 +0100

    added monolithic solver options

--- sw_energy.py
+++ sw_energy.py
@@ -188,7 +188,39 @@ sparameters_lu = {
     "fieldsplit_1_Mp_pc_type": 'lu',
 }
 
-
+sparameters = {
+    "snes_monitor": None,
+    "mat_type": "matfree",
+    "ksp_type": "fgmres",
+    "ksp_monitor_true_residual": None,
+    "ksp_converged_reason": None,
+    "ksp_atol": 1e-8,
+    "ksp_rtol": 1e-8,
+    "ksp_max_it": 400,
+    "pc_type": "mg",
+    "pc_mg_cycle_type": "v",
+    "pc_mg_type": "multiplicative",
+    "mg_levels_ksp_type": "gmres",
+    "mg_levels_ksp_max_it": 3,
+    #"mg_levels_ksp_convergence_test": "skip",
+    "mg_levels_pc_type": "python",
+    "mg_levels_pc_python_type": "firedrake.PatchPC",
+    "mg_levels_patch_pc_patch_save_operators": True,
+    "mg_levels_patch_pc_patch_partition_of_unity": True,
+    "mg_levels_patch_pc_patch_sub_mat_type": "seqdense",
+    "mg_levels_patch_pc_patch_construct_codim": 0,
+    "mg_levels_patch_pc_patch_construct_type": "vanka",
+    "mg_levels_patch_pc_patch_local_type": "additive",
+    "mg_levels_patch_pc_patch_precompute_element_tensors": True,
+    "mg_levels_patch_pc_patch_symmetrise_sweep": False,
+    "mg_levels_patch_sub_ksp_type": "preonly",
+    "mg_levels_patch_sub_pc_type": "lu",
+    "mg_levels_patch_sub_pc_factor_shift_type": "nonzero",
+    "mg_coarse_pc_type": "python",
+    "mg_coarse_pc_python_type": "firedrake.AssembledPC",
+    "mg_coarse_assembled_pc_type": "lu",
+    "mg_coarse_assembled_pc_factor_mat_solver_type": "mumps",
+}
  
 dt = 60*60*args.dt
 dT.assign(dt)
