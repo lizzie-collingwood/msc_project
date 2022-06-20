d5a5d665faa9a617eabe527c1ae1f16848449ee5
Lizzie Collingwood <ljc21@ic.ac.uk>
2022-06-20 10:46:14 +0100

    removed spurious comments

--- sw_energy.py
+++ sw_energy.py
@@ -1,5 +1,4 @@
 import firedrake as fd
-# import transfer
 #get command arguments
 from petsc4py import PETSc
 PETSc.Sys.popErrorHandler()
@@ -258,18 +257,6 @@ t = 0.
 
 nprob = fd.NonlinearVariationalProblem(p_vel_eqn, Unp1)
 nsolver = fd.NonlinearVariationalSolver(nprob, solver_parameters=mg_parameters)
-# vtransfer = transfer.ManifoldTransfer()
-# tm = fd.TransferManager()
-# transfers = {
-#     # V0.ufl_element(): (vtransfer.prolong, vtransfer.restrict,
-#     #                    vtransfer.inject),
-#     V1.ufl_element(): (vtransfer.prolong, vtransfer.restrict,
-#                        vtransfer.inject),
-#     V2.ufl_element(): (vtransfer.prolong, vtransfer.restrict,
-#                        vtransfer.inject)
-# }
-# transfermanager = fd.TransferManager(native_transfers=transfers)
-# nsolver.set_transfer_manager(transfermanager)
 
 dmax = args.dmax
 hmax = 24*dmax
