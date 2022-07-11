ae8a2940fbc5b04a00f40fa92148462e937b5782
Lizzie Collingwood <ljc21@ic.ac.uk>
2022-07-11 17:02:56 +0100

    arg options printed to txt

--- sw_energy.py
+++ sw_energy.py
@@ -316,8 +316,10 @@ with open(name+'.json', 'w') as f:
     json.dump(simdata, f)
 
 # Write options to text file.
+allargs = parser.parse_args()
+options = vars(allargs)
 with open(name+'_options.txt', 'w') as f:
-    f.write('\n'.join(list(args)))
+    f.write('\n'.join(list(options)))
 
 PETSc.Sys.Print("Iterations", itcount,
                 "dt", dt,
