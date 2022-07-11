2650e815452b8a947d83d9e9d8fed173fb03d164
Lizzie Collingwood <ljc21@ic.ac.uk>
2022-07-11 17:15:50 +0100

    fix to txt output

--- sw_energy.py
+++ sw_energy.py
@@ -317,7 +317,7 @@ with open(name+'.json', 'w') as f:
 
 # Write options to text file.
 with open(name+'_options.txt', 'w') as f:
-    f.write(args)
+    f.write('\n'.join(vars(args)))
 
 PETSc.Sys.Print("Iterations", itcount,
                 "dt", dt,
