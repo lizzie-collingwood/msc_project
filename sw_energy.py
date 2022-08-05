92e8a7a8437d0ac01227e8312a1a0272e2dbfb88
Lizzie Collingwood <ljc21@ic.ac.uk>
2022-08-05 08:42:04 +0100

    rearranged

--- sw_energy.py
+++ sw_energy.py
@@ -221,8 +221,6 @@ mg_parameters = {
     # "mg_coarse_assembled_pc_factor_mat_solver_type": "superlu_dist", # ###
 }
 
-# Solve for D1
-D1_eqn = phi*(D1 - D0 + dT*fd.div(F1))*dx
 
 
 # Time step size [s]
@@ -230,14 +228,15 @@ dt = 60*60*args.dt
 dT.assign(dt)
 t = 0.
 
-
-D1prob = fd.NonlinearVariationalProblem(D1_eqn, D1)
-D1params = {'ksp_type':'cg'}
-D1solver = fd.NonlinearVariationalSolver(D1prob, solver_parameters=D1params)
-
 # Nonlinear solver
 nprob = fd.NonlinearVariationalProblem(p_vel_eqn, Unp1)
 nsolver = fd.NonlinearVariationalSolver(nprob, solver_parameters=mg_parameters)
+
+# Solve for D1
+D1_eqn = phi*(D1 - D0 + dT*fd.div(F1))*dx
+D1prob = fd.NonlinearVariationalProblem(D1_eqn, D1)
+D1params = {'ksp_type':'cg'}
+D1solver = fd.NonlinearVariationalSolver(D1prob, solver_parameters=D1params)
 
 dmax = args.dmax 
 hmax = 24*dmax
