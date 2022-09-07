3852dce3fe9e336b2914f3a99e55d41e1322d075
Lizzie Collingwood <ljc21@ic.ac.uk>
2022-09-07 10:19:28 +0100

    fix attempt gbal_eqn

--- sw_energy.py
+++ sw_energy.py
@@ -282,7 +282,7 @@ vort_problem = fd.LinearVariationalProblem(fd.lhs(vrt_eqn), fd.rhs(vrt_eqn), vor
 vortsolver = fd.LinearVariationalSolver(vort_problem, solver_parameters=cg_prms)
 
 # Compute geostrophic balance error
-gbal_eqn = (g*fd.grad(D0+b) + f*fd.perp(un))*dx
+gbal_eqn = (g*fd.grad(Dh+b) + f*fd.perp(uh))*dx
 gbal0 = fd.assemble(gbal_eqn)
 
 # Write initial fields into a file which can be interpreted by software ParaView
